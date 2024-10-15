d9840bcf40cd8c05b182828e68d89e60608c714d
Majda Mnasri <majdamnasri@gmail.com>
2024-10-15 11:14:51 +0100

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //my first commit
+        //my second commit
         [Key]
         public int CartItemId { get; set; }
 
